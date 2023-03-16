dfeaebdf1ed510bae1838bff7be5297b943f661f
Dane Edwards <Daneedw@gmail.com>
2023-03-16 13:11:39 -0500

    adding put

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const movies = [{title:"The Matrix", Genre: "Sci-Fi" }, {title:"Star Wars", Genre: "Space Opera" }];
+const movies = [{title:"The Matrix", Genre: "Sci-Fi", id:1 }, {title:"Star Wars", Genre: "Space Opera" , id:2}];
+
+let currentId= 3;
 
 app.post("/movies", (req, res) => {
     movies.push(req.body)
+    res.json(movies);
+})
+app.put("/movies/:id", (req, res) => {
+    
+   movies =  movies.map(movie =>{
+
+
+        if(movie.id === req.params.id){
+
+            return {...req.params.body, id: currentId};
+           
+
+        }
+
+        else return movie;
+
+    })
+   currentId++;
+
     res.json(movies);
 })
 
