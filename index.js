b716091fddd100012283a48f26e087d27d890f38
Dane Edwards <Daneedw@gmail.com>
2023-03-16 13:18:01 -0500

    put not working correctly here...

--- index.js
+++ index.js
@@ -7,30 +7,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const movies = [{title:"The Matrix", Genre: "Sci-Fi", id:1 }, {title:"Star Wars", Genre: "Space Opera" , id:2}];
+let movies = [{ title: "The Matrix", Genre: "Sci-Fi", id: 1 }, { title: "Star Wars", Genre: "Space Opera", id: 2 }];
 
-let currentId= 3;
+let currentId = 3;
 
 app.post("/movies", (req, res) => {
-    movies.push(req.body)
+
+    movies.push({ ...req.body, id: currentId })
+    currentId++;
+
     res.json(movies);
 })
 app.put("/movies/:id", (req, res) => {
-    
-   movies =  movies.map(movie =>{
+console.log(req.params.id)
+console.log(req.params.body)
+
+    movies = movies.map(movie => {
 
 
-        if(movie.id === req.params.id){
+        if (movie.id === req.params.id) {
+            console.log("change")
+            return { ...req.params.body };
 
-            return {...req.params.body, id: currentId};
-           
 
         }
 
         else return movie;
 
     })
-   currentId++;
+
 
     res.json(movies);
 })
